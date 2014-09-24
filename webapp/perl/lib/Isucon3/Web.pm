9aac125dd89609441ecaf17caede92d62cd35ad0
trapple <trapplejp@gmail.com>
2014-09-24 09:16:35 +0000

    N+1クエリを最適化

--- webapp/perl/lib/Isucon3/Web.pm
+++ webapp/perl/lib/Isucon3/Web.pm
@@ -110,14 +110,14 @@ get '/' => [qw(session get_user)] => sub {
         'SELECT count(*) FROM memos WHERE is_private=0'
     );
     my $memos = $self->dbh->select_all(
-        'SELECT * FROM memos WHERE is_private=0 ORDER BY created_at DESC, id DESC LIMIT 100',
+        'SELECT memos.*,users.username FROM memos JOIN users ON memos.user = users.id WHERE is_private=0 ORDER BY created_at DESC, id DESC LIMIT 100',
     );
-    for my $memo (@$memos) {
-        $memo->{username} = $self->dbh->select_one(
-            'SELECT username FROM users WHERE id=?',
-            $memo->{user},
-        );
-    }
+    #for my $memo (@$memos) {
+    #    $memo->{username} = $self->dbh->select_one(
+    #        'SELECT username FROM users WHERE id=?',
+    #        $memo->{user},
+    #    );
+    #}
     $c->render('index.tx', {
         memos => $memos,
         page  => 0,
@@ -132,18 +132,18 @@ get '/recent/:page' => [qw(session get_user)] => sub {
         'SELECT count(*) FROM memos WHERE is_private=0'
     );
     my $memos = $self->dbh->select_all(
-        sprintf("SELECT * FROM memos WHERE is_private=0 ORDER BY created_at DESC, id DESC LIMIT 100 OFFSET %d", $page * 100)
+        sprintf("SELECT memos.*,users.username FROM memos JOIN users ON memos.user = users.id WHERE is_private=0 ORDER BY created_at DESC, id DESC LIMIT 100 OFFSET %d", $page * 100)
     );
     if ( @$memos == 0 ) {
         return $c->halt(404);
     }
 
-    for my $memo (@$memos) {
-        $memo->{username} = $self->dbh->select_one(
-            'SELECT username FROM users WHERE id=?',
-            $memo->{user},
-        );
-    }
+    #for my $memo (@$memos) {
+    #    $memo->{username} = $self->dbh->select_one(
+    #        'SELECT username FROM users WHERE id=?',
+    #        $memo->{user},
+    #    );
+    #}
     $c->render('index.tx', {
         memos => $memos,
         page  => $page,
